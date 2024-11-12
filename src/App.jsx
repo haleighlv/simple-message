b27087a020f0613fefb663466ae13be10e1e0a69
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-11-13 08:19:33 +1000

    simplemessagetask

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  const [message, setMessage] = React.useState("Javascript is so cool.");
+
+  function handleClick() {
+    setMessage("I love React");
+  }
+  
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Update the message!</button>
+    </div>
+  );
+}
+
+export default App;
